e8d9beba68be988d79e51aa3f00c526685909c8a
nissha29 <nishakashyap2907@gmail.com>
2025-05-24 18:43:31 +0530

    reoved unused variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Underdog } from "next/font/google";
+import { Underdog } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const underDog = Underdog({
   variable: "--font-underdog",
