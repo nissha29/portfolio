e5041787bd823578b738f2fbec8a09780f2c432f
Tushar Verma <tusharsoni014@gmail.com>
2025-11-29 18:27:54 +0530

    added OG image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,39 @@ import CommandHint from "@/components/CommandHint";
 const inconsolata = Inconsolata({
   variable: "--font-quattrocento",
   subsets: ["latin"],
-  weight: ["400"]
-})
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Nisha | Portfolio",
-  description: "Hi, I’m Nisha—a Full Stack Developer with a passion for building seamless, intuitive applications that make technology accessible and enjoyable.",
+  description:
+    "Hi, I'm Nisha—a Full Stack Developer with a passion for building seamless, intuitive applications that make technology accessible and enjoyable.",
+  metadataBase: new URL("https://nissha.space"),
+  openGraph: {
+    title: "Nisha | Portfolio",
+    description:
+      "Hi, I'm Nisha—a Full Stack Developer with a passion for building seamless, intuitive applications that make technology accessible and enjoyable.",
+    url: "https://nissha.space",
+    siteName: "Nisha's Portfolio",
+    images: [
+      {
+        url: "/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Nisha | Full Stack Developer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nisha | Portfolio",
+    description:
+      "Hi, I'm Nisha—a Full Stack Developer with a passion for building seamless, intuitive applications that make technology accessible and enjoyable.",
+    creator: "@nissha29",
+    images: ["/opengraph-image.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -26,21 +53,19 @@ export default function RootLayout({
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "Person",
-    "name": "Nisha",
-    "url": "https://nissha.space",
-    "jobTitle": "Full Stack Developer",
-    "sameAs": [
+    name: "Nisha",
+    url: "https://nissha.space",
+    jobTitle: "Full Stack Developer",
+    sameAs: [
       "https://github.com/nissha29",
       "https://linkedin.com/in/nisha-yadav-698329230",
-      "https://twitter.com/nissha29"
-    ]
+      "https://twitter.com/nissha29",
+    ],
   };
 
   return (
     <html lang="en">
-      <body
-        className={` ${inconsolata.className} antialiased`}
-      >
+      <body className={` ${inconsolata.className} antialiased`}>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
@@ -48,9 +73,7 @@ export default function RootLayout({
         <ScrollProgress />
         <CommandMenu />
         <CommandHint />
-        <SmoothScroll>
-          {children}
-        </SmoothScroll>
+        <SmoothScroll>{children}</SmoothScroll>
         <Analytics />
       </body>
     </html>
